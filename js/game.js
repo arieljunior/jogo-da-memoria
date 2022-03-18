b2bf6575251e3be5e63374c119c8f96dd72293f9
Ariel Junior <ariel.junior@actdigital.com>
2022-03-18 11:31:18 -0300

    refactor

--- js/game.js
+++ js/game.js
@@ -1,81 +1,93 @@
 const buttonStartGameElement = document.querySelector("#btn-start-game");
+const boardGameElement = document.querySelector("#board");
+const pointsElement = document.querySelector("#points");
+const hitsElement = document.querySelector("#hits");
+const errorsElement = document.querySelector("#errors");
 
-const historyIndexsMatcheds = [];
+let historyIndexsMatcheds = [];
 const indexesCompare = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-let cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let cardsPosition = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let points = 0;
+let errors = 0;
+let hits = 0;
 let firstCard, secondCard;
-
 let blockBoard = false;
 
 const shuffleCards = () => {
-  cards = cards.sort(() => Math.random() - 0.5);
+  cardsPosition = cardsPosition.sort(() => Math.random() - 0.5);
 };
 
 const handleClickCard = (element, index) => {
+  if (historyIndexsMatcheds.indexOf(index) > -1) return;
 
-  if(historyIndexsMatcheds.indexOf(index) > -1) return;
+  if (blockBoard) return;
 
-  if(blockBoard) return;
-
-  if(!firstCard){
+  if (!firstCard) {
     firstCard = { element, index };
-  }else if(!secondCard){
-    secondCard = { element, index }; 
+  } else if (!secondCard) {
+    secondCard = { element, index };
   }
 
   element.style.backgroundImage = `url('./img/${index}.png')`;
 
-  if(firstCard && secondCard){
+  if (firstCard && secondCard) {
     testCards();
   }
-
 };
 
+const isFinishGame = () => hits === cardsPosition.length / 2;
+
 const testCards = () => {
-  
   blockBoard = true;
 
   setTimeout(() => {
     let isMatch = isMatchCards();
 
-    let message;
-    if(isMatch){
-      message = "você acertou!";
-      handleMatchedCards();
-    }else{
-      message = "você errou!";
+    if (isMatch) {
+      hits++;
+      points++;
+      historyIndexsMatcheds.push(firstCard.index);
+      historyIndexsMatcheds.push(secondCard.index);
+    } else {
+      errors++;
       points--;
-      hideCards();
     }
 
-    alert(message);
-    
-    blockBoard = false;
+    updateBoard(!isMatch);
+    updateInfo();
+
 
     firstCard = null;
     secondCard = null;
+
+    if (isFinishGame()) {
+      buttonStartGameElement.disabled = false;
+      return;
+    }
+
+    blockBoard = false;
   }, 500);
-}
+};
 
-const handleMatchedCards = () => {
-  firstCard.element.style.opacity = '0.5';
-  secondCard.element.style.opacity = '0.5';
+const updateBoard = (hideCards = false) => {
+  if (hideCards) {
+    const urlImage = "url('./img/card-back.png')";
+    firstCard.element.style.backgroundImage = urlImage;
+    secondCard.element.style.backgroundImage = urlImage;
+  } else {
+    firstCard.element.style.opacity = "0.5";
+    secondCard.element.style.opacity = "0.5";
+  }
+};
 
-  historyIndexsMatcheds.push(firstCard.index);
-  historyIndexsMatcheds.push(secondCard.index);
-
-  points++;
-}
-
-const hideCards = () => {
-  const urlImage = "url('./img/card-back.png')";
-  firstCard.element.style.backgroundImage = urlImage;
-  secondCard.element.style.backgroundImage = urlImage;
+const updateInfo = () => {
+  pointsElement.innerHTML = points;
+  hitsElement.innerHTML = hits;
+  errorsElement.innerHTML = errors;
 }
 
 const isMatchCards = () => {
-  if(!firstCard || !secondCard){
+  if (!firstCard || !secondCard) {
     return false;
   }
 
@@ -83,17 +95,44 @@ const isMatchCards = () => {
   const indexSecondCard = secondCard.index;
 
   return indexesCompare[indexFirstCard] === indexesCompare[indexSecondCard];
+};
+
+const drawBoard = () => {
+  boardGameElement.innerHTML = "";
+  cardsPosition.forEach(() => {
+    let divCard = document.createElement('div');
+    divCard.classList.add("card");
+    divCard.classList.add("card-back");
+    boardGameElement.appendChild(divCard);
+  })
+}
+
+const resetGame = () => {
+  points = 0;
+  errors = 0;
+  hits = 0;
+  firstCard = null;
+  secondCard = null;
+  blockBoard = false;
+  historyIndexsMatcheds = [];
+  updateInfo();
 }
 
 function startGame() {
   buttonStartGameElement.disabled = true;
 
+  if(isFinishGame()){
+    resetGame();
+  }
   shuffleCards();
+  drawBoard();
 
   const cardsElement = document.querySelectorAll(".card");
 
   cardsElement.forEach((cardElement, index) => {
-    cardElement.addEventListener("click", () => handleClickCard(cardElement, cards[index]));
+    cardElement.addEventListener("click", () =>
+      handleClickCard(cardElement, cardsPosition[index])
+    );
   });
 }
 
